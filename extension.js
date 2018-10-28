1203b05211bca6685a2b80a0a26880c621c5b1a8
Lucas Vogel <lucas.vogel96@web.de>
2018-10-28 21:13:51 +0100

    first working Prototype

--- extension.js
+++ extension.js
@@ -1,29 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-async function activate(context) {
+const Taskbar = require( "./lib/taskbar");
+let taskbar = new Taskbar();
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+function activate(context) {
+
     console.log('Congratulations, your extension "agsbs" is now active!');
-
+    vscode.window.showInformationMessage('AGSBS is Active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    
+    let disposable = vscode.commands.registerCommand('extension.agsbs', function () {
+       main(context);
+    });
+    
+     console.log(taskbar);
+    vscode.window.onDidChangeActiveTextEditor(taskbar.update(context), this);
+    context.subscriptions.push(taskbar); //For Disposables
+    context.subscriptions.push(disposable);
+}
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
 
-         //layout = { orientation: 0, groups: [{ groups: [{}, {}], size: 0.5 }, { groups: [{}, {}], size: 0.5 }] };
-         
+exports.activate = activate;
+
+// this method is called when the extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
 
 
-        const panel = vscode.window.createWebviewPanel(
+
+
+function main (context){
+
+         const panel = vscode.window.createWebviewPanel(
             'catCoding', // Identifies the type of the webview. Used internally
             "Cat Coding", // Title of the panel displayed to the user
             //vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
@@ -33,16 +45,7 @@ async function activate(context) {
         panel.webview.html = getWebviewContent();
         layout = { orientation: 1, groups: [{  size: 0.85 }, {  size: 0.15 }] };
          vscode.commands.executeCommand('vscode.setEditorLayout', layout);
-    });
-
-    context.subscriptions.push(disposable);
 }
-exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
 
 function getWebviewContent() {
     return `<!DOCTYPE html>
---
+++ lib/taskbar.ts
@@ -0,0 +1,128 @@
+const vscode = require('vscode');
+const util = require('util');
+const { spawn } = require( 'child_process' );
+
+export class Taskbar {
+     panel;
+     panelIsVisible=false;
+     mediaDirectory = 'media'
+
+    constructor() { 
+       this.update();
+        
+    }
+
+    _showTaskbar() {
+        //vscode.window.showInformationMessage('show');
+        if(this.panelIsVisible==true){ //if webView is already visible
+            return;
+        }
+        var panel = vscode.window.createWebviewPanel(
+            'agsbstaskbar', // Identifies the type of the webview. Used internally
+            "AGSBS Toolbar", // Title of the panel displayed to the user
+            //vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+            vscode.ViewColumn.Two,
+            { 
+                enableScripts: true
+            } // Webview options. More on these later.
+        );
+        panel.webview.html = this.getWebviewContent();
+        this.panelIsVisible =true;
+        var layout = { orientation: 1, groups: [{  size: 0.85 }, {  size: 0.15 }] };
+         vscode.commands.executeCommand('vscode.setEditorLayout', layout);
+            this.panel=panel;
+
+
+         panel.onDidDispose(() => {
+            // When the panel is closed, cancel any future updates to the webview content
+            //vscode.window.showInformationMessage('DISPOSED');
+            this.panelIsVisible =false; //When panel is closed
+        }, null);
+
+        panel.webview.onDidReceiveMessage(message => {
+            //vscode.window.showInformationMessage(message.text);
+            //MESSAGE HAS ALSO message.command
+            switch (message.text) {
+                case 'testbutton':
+                    //vscode.window.showErrorMessage(message.text);
+                    this.ls();
+                    return;
+            }
+        }, undefined);
+    }
+    _hideTaskbar(panel) {
+        //vscode.window.showInformationMessage('hide');
+        //vscode.window.showInformationMessage('AGSBS Deactivated!');
+        if(this.panelIsVisible==true){
+            //vscode.window.showInformationMessage('close');
+        this.panel.dispose();
+        }
+        this.panelIsVisible =false;
+    }
+    
+    update() {
+        //Taskbar.prototype.ls();
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        let doc = editor.document;
+        if (doc.languageId === "markdown") {
+            Taskbar.prototype._showTaskbar();
+            console.log(this.panel);
+        } else {
+            Taskbar.prototype._hideTaskbar(this.panel);
+        }
+    }
+    ls() {    
+            var ls = spawn( 'matuc_js', [ 'version' ] );//var ls = spawn( 'ls', [ '-lh', '/usr' ] );
+            ls.stdout.on( 'data', data => {
+                vscode.window.showInformationMessage( `stdout: ${data}` );
+            } );
+
+            ls.stderr.on( 'data', data => {
+                console.log( `stderr: ${data}` );
+            } );
+
+            ls.on( 'close', code => {
+                console.log( `child process exited with code ${code}` );
+            } );
+      }
+      
+    
+    
+
+     getWebviewContent() {
+        return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cat Coding</title>
+    </head>
+    <body>
+        <button onclick="sendMessage('testbutton')">Test</button>
+        <h1 id="output">Unloaded</h1>
+        <script>
+        const output = document.getElementById('output');
+        output.innerHTML= "Loaded";
+        
+            const vscode = acquireVsCodeApi();
+
+            function sendMessage(message){
+                vscode.postMessage({
+                    command: 'button',
+                    text: message
+                })
+            }
+            
+    </script>
+    </body>
+    </html>`;
+    }
+
+}
+
+
+
+module.exports = Taskbar;
