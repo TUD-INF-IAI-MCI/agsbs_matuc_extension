c493cdea60a3f83b67397366b00836d9d89ad4bc
Jens V <jens781@googlemail.com>
2021-06-14 14:46:45 +0200

    cleanup

--- out/languages/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TestLanguage {
-    constructor() {
-    }
-}
-exports.default = TestLanguage;
-//# sourceMappingURL=test.js.map
